118804e2a71c9ee654e5e0fafd7bde4536456a61
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-19 23:34:42 +0400

    bitfinexAPI

---
+++ BitfinexAPI/BitfinexAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BitfinexAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticker ticker = new Ticker();
+            ticker.Market();
+            Console.WriteLine(ticker.ValueAsk);
+            Console.WriteLine(ticker.ValueBid);
+        }
+    }
+}
---
+++ BitfinexAPI/BitfinexAPI/Ticker.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace BitfinexAPI
+{
+    public class Ticker
+    {
+        public decimal ValueAsk { get; set; }
+        public decimal ValueBid { get; set; }
+
+        public void Market()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.bitfinex.com/v1/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applaction/json"));
+            HttpResponseMessage responseMessage = client.GetAsync($"pubticker/btcusd").Result
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var product = responseMessage.Content.ReadAsStringAsync().Result;
+                JObject jObject = JObject.Parse(product);
+                ValueAsk = (decimal)jObject["ask"];
+                ValueBid = (decimal)jObject["bid"];
+            }
+        }
+    }
+}
